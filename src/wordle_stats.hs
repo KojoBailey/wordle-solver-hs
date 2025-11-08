2eb0069e122243ab4b5258e7d0e290dd405b972a
Kojo Bailey <kojomolojo@gmail.com>
2025-11-08 15:36:56 +0000

    enable writing of file

--- src/wordle_stats.hs
+++ src/wordle_stats.hs
@@ -12,9 +12,13 @@ calculate_positional_letter_frequency ws
   | otherwise   = calculate_letter_frequency empty_list (map head ws) : calculate_positional_letter_frequency (map tail ws)
   where empty_list = [(c, 0) | c <- ['a'..'z']]
 
-calculate_likelihood :: [String] -> [(Char, Integer)] -> [(String, Integer)]
-calculate_likelihood ws freqs = map (\ w -> (w, sum [map snd freqs !! char_to_int c | c <- w])) ws
+
+calculate_likelihood_word :: [[(Char, Integer)]] -> String -> Integer
+calculate_likelihood_word freqs w = sum [map snd freq !! char_to_int c | (c, freq) <- zip w freqs]
   where char_to_int c = ord c - ord 'a'
+
+calculate_likelihood :: [String] -> [[(Char, Integer)]] -> [(String, Integer)]
+calculate_likelihood ws freqs = [(w, calculate_likelihood_word freqs w) | w <- ws]
 
 
 main :: IO ()
@@ -24,15 +28,14 @@ main = do
   let words = lines contents
 
   putStrLn "Calculating letter frequencies..."
-  print $ calculate_positional_letter_frequency words
-  -- let letter_frequencies = calculate_letter_frequency [(c, 0) | c <- ['a'..'z']] words
-  -- print letter_frequencies
+  let positional_letter_frequencies = calculate_positional_letter_frequency words
+  print positional_letter_frequencies
 
-  -- putStrLn "Calculating likelihood score for each word..."
-  -- let result = calculate_likelihood words letter_frequencies
+  putStrLn "Calculating likelihood score for each word..."
+  let result = calculate_likelihood words positional_letter_frequencies
 
-  -- let output_filename = "wordle-stats.txt"
-  -- putStrLn $ "Writing data to " ++ output_filename ++ "..."
-  -- writeFile output_filename (show result)
+  let output_filename = "wordle-stats.txt"
+  putStrLn $ "Writing data to " ++ output_filename ++ "..."
+  writeFile output_filename (show result)
 
   putStrLn "Complete!"
